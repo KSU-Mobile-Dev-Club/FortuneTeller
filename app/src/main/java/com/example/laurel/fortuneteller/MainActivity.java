46925492b52dfc9ddbf53ee77019e032f0be1324
Laurel <laurel.beth.thomson@gmail.com>
2018-07-24 19:36:21 -0500

    Created Data Entry Activity and hooked up both buttons on the Main Activity to it

--- app/src/main/java/com/example/laurel/fortuneteller/MainActivity.java
+++ app/src/main/java/com/example/laurel/fortuneteller/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.laurel.fortuneteller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.yesButton).setOnClickListener(this);
+        findViewById(R.id.noButton).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(this, DataEntryActivity.class));
     }
 }
