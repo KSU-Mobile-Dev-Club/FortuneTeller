46a2379931f4f9229d349ad6d816535c187eecba
Laurel <laurel.beth.thomson@gmail.com>
2018-07-24 20:02:21 -0500

    Added question placeholders and number picker to Data Entry Activity

--- app/src/main/java/com/example/laurel/fortuneteller/DataEntryActivity.java
+++ app/src/main/java/com/example/laurel/fortuneteller/DataEntryActivity.java
@@ -2,6 +2,7 @@ package com.example.laurel.fortuneteller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.NumberPicker;
 
 public class DataEntryActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class DataEntryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_entry);
+
+        NumberPicker meetingCountPicker = findViewById(R.id.meetingCountPicker);
+        meetingCountPicker.setMinValue(0);
+        meetingCountPicker.setMaxValue(10);
     }
 }
