4467ce5368ad08c438f04a6db41da0d86d3b582e
= <stevenblair@ksu.edu>
2018-07-26 23:00:49 -0500

    Added method to fetch question answers

--- app/src/main/java/com/example/laurel/fortuneteller/DataEntryActivity.java
+++ app/src/main/java/com/example/laurel/fortuneteller/DataEntryActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.NumberPicker;
+
+import java.util.HashMap;
 
 public class DataEntryActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -24,10 +27,25 @@ public class DataEntryActivity extends AppCompatActivity implements View.OnClick
         meetingCountPicker.setMaxValue(10);
     }
 
+    private HashMap<String, String> get_answers() {
+        HashMap<String, String> data = new HashMap<>();
+
+        data.put("name", ((EditText) findViewById(R.id.question1)).getText().toString());
+        data.put("email", ((EditText) findViewById(R.id.question2)).getText().toString());
+        data.put("meeting_count", Integer.toString(meetingCountPicker.getValue()));
+
+        return data;
+    }
+
     @Override
     public void onClick(View v) {
+        // fetch answers
+        HashMap<String, String> data = get_answers();
+
+        // launch FortuneActivity
         Intent intent = FortuneActivity
                 .makeIntent(this, meetingCountPicker.getValue());
         startActivity(intent);
     }
+
 }
