dab2ef7cbf8667645e3b2bb3002275c191b3592b
Laurel <laurel.beth.thomson@gmail.com>
2018-08-15 15:12:55 -0500

    Redesigning activity_main and adding animation

--- app/src/main/java/com/example/laurel/fortuneteller/MainActivity.java
+++ app/src/main/java/com/example/laurel/fortuneteller/MainActivity.java
@@ -1,24 +1,36 @@
 package com.example.laurel.fortuneteller;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.view.animation.Animation;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private AnimationDrawable animation;
+    private static final int ANIMATION_TIME_OUT = 1200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        animation = (AnimationDrawable) findViewById(R.id.crystal_ball).getBackground();
+        findViewById(R.id.yes_button).setOnClickListener(this);
 
-        findViewById(R.id.yesButton).setOnClickListener(this);
-        findViewById(R.id.noButton).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        startActivity(new Intent(this, DataEntryActivity.class));
+        animation.start();
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                animation.stop();
+                startActivity(new Intent(MainActivity.this, DataEntryActivity.class));
+            }
+        }, ANIMATION_TIME_OUT);
     }
 }
